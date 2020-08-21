e16c974d47ec49c66b741bfc2f3de31f1e20a2ef
Wes Dixon <jdixon3@ycp.edu>
2020-08-21 08:13:07 -0700

    first few exercises in the documentation small problems

---
+++ class.rb
@@ -0,0 +1,10 @@
+
+
+
+# Access CORE then scroll down to Path (File) under methods
+
+# File.path (class methods call on class) 
+# puts File.path('bin')        ex:  We return the string representation of the path
+
+# File#path is an instance method which call on objects
+# f = File.new('my-file.txt')      ex: We are calling the instance method File#path, on an object of the file class named f.
---
+++ redux.rb
@@ -0,0 +1,12 @@
+
+
+require 'date'
+
+
+d = Date.parse('3rd Feb 2001')
+
+puts d.year
+puts d.mon
+puts d.mday
+puts d.wday
+puts d += 1
